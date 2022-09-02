1fbcd4e7f24ed9fe1d0f4613534fdf2831e713da
Ngowwa-Ziro <joseph.ngowwa@student.moringaschool.com>
2022-09-02 10:51:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let replace = /[.,'!?\- \"]/g;
+  let text = word.replace(replace, '').toUpperCase();
+  for (let i = 0; i < Math.floor(text.length / 2) - 1; i++) {
+    if (text.charAt(i) == text.charAt(text.length - 1 - i)) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
 */
-
+function isPalindrome(str) {
+  return str === str.split('').reverse().join('');
+}
 /*
   Add written explanation of your solution here
 */
